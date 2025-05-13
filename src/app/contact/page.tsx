61440006ca46398e078172b3d7a1d21c2a33ab9b
SGTTW <88731480+SGTTW@users.noreply.github.com>
2025-05-13 01:29:03 +0100

    update contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import Container from "@/components/common/Container";
+
+export default function ContactPage() {
+  return (
+    <Container>
+      <div className="py-12">
+        <h1 className="text-4xl font-bold text-center mb-12">Contact Page</h1>
+        {/* <TestimonialsSection testimonials={testimonials} /> */}
+      </div>
+    </Container>
+  );
+}
