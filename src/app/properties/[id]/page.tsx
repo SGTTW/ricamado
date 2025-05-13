199d843caa4c42e0fb5af727487bc0b4ab30d8ee
SGTTW <88731480+SGTTW@users.noreply.github.com>
2025-05-13 01:32:25 +0100

    update contact page

---
+++ src/app/properties/[id]/page.tsx
@@ -0,0 +1,13 @@
+import Container from "@/components/common/Container";
+
+export default function PropertyIDPage() {
+  return (
+    <Container>
+      <div className="py-12">
+        <h1 className="text-4xl font-bold text-center mb-12">
+          Property id page
+        </h1>
+      </div>
+    </Container>
+  );
+}
---
+++ src/app/why/page.tsx
@@ -0,0 +1,11 @@
+import Container from "@/components/common/Container";
+
+export default function WhyPage() {
+  return (
+    <Container>
+      <div className="py-12">
+        <h1 className="text-4xl font-bold text-center mb-12">why page</h1>
+      </div>
+    </Container>
+  );
+}
